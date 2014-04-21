e66f62df9977b275be2a49eb6be53137212a533c
Badia Daamash <bjdaamash@pbs.org>
2014-04-20 21:39:43 -0400

    added .gitignore

--- js/main.js
+++ js/main.js
@@ -94,7 +94,6 @@ $(document).ready(function(){
         $('html, body').stop().animate({
             scrollTop: ($($anchor.attr('href')).offset().top -50)
         }, 1500);
-
         e.preventDefault();
     });
     
